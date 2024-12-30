799da47ab88c36d4eb6bbd862551919386e49f8b
Lohitha-465 <143075756+Lohitha-465@users.noreply.github.com>
2024-12-30 19:40:25 +0530

    Update Footer.js

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -80,7 +80,7 @@ export default function Footer() {
         </div>
         <div className="mt-8 md:order-1 md:mt-0">
           <p className="text-center text-xs leading-5 text-gray-500">
-            &copy; 2023 Thanush Chowdary, Inc. All rights reserved.
+            &copy; 2023 Lohitha Koneru, Inc. All rights reserved.
           </p>
         </div>
       </div>
